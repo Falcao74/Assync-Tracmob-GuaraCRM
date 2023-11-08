a4c2b3bb63da8ddefe11235c2af46aab981bdde5
Guaraci Falcão <53480221+Falcao74@users.noreply.github.com>
2023-11-08 09:14:10 -0300

    Update load.py
    Refactored

--- load.py
+++ load.py
@@ -1,28 +1,18 @@
 import pandas as pd
-import pymysql
-from sqlalchemy import create_engine
-from settings import db_server, db_name, db_user, db_password, db_port
 
 class Load:
+    def __init__(self, normalized_data, engine):
+        self.pd_constituents = pd.json_normalize(normalized_data.constituents, sep='_')
+        self.pd_donations = pd.json_normalize(normalized_data.donations, sep='_')
+        self.pd_payments = pd.json_normalize(normalized_data.payments, sep='_')
+        self.pd_adresses = pd.json_normalize(normalized_data.adresses, sep='_')
+        self.engine = engine
+        # usando list comprehension, eu retiro dos dados qualquer coluna que tenha um nome maior do que o limite.
+        limit = 24
+        self.pd_payments = self.pd_payments.loc[:, [x for x in self.pd_payments.columns if len(x) < limit]]
     
-    def __init__(self):
-        self.pd_constituents: pd.DataFrame = pd.DataFrame()
-        self.pd_donations: pd.DataFrame = pd.DataFrame()
-        self.pd_payments: pd.DataFrame = pd.DataFrame()
-        self.pd_adresses: pd.DataFrame = pd.DataFrame()
-        self._engine = create_engine(f'mysql+pymysql://{db_user}:{db_password}@{db_server}:{db_port	}/{db_name}?charset=utf8mb4')
-        
-    def prepare_to_load(self, data) -> None:
-        self.pd_constituents = pd.json_normalize(data.constituents, sep='_')
-        self.pd_donations = pd.json_normalize(data.donations, sep='_')
-        self.pd_payments = pd.json_normalize(data.payments, sep='_')
-        self.pd_adresses = pd.json_normalize(data.adresses, sep='_')
-        # using list comprehension, to avoid bad columns format.
-        limit = 40
-        self.pd_payments = self.pd_payments.loc[:, [x for x in self.pd_payments.columns if len(x) < limit]]   
-        
-    def perform(self) -> None:
-        self.pd_constituents.to_sql('constituents', self._engine, if_exists='append', index=False)
-        self.pd_donations.to_sql('donations', self._engine, if_exists='append', index=False)
-        self.pd_payments.to_sql('payments', self._engine, if_exists='append', index=False)
-        self.pd_adresses.to_sql('adresses', self._engine, if_exists='append', index=False)
+    def perform(self):
+        self.pd_constituents.to_sql('constituents', self.engine, if_exists='append', index=False)
+        self.pd_donations.to_sql('donations', self.engine, if_exists='append', index=False)
+        self.pd_payments.to_sql('payments', self.engine, if_exists='append', index=False)
+        self.pd_adresses.to_sql('adresses', self.engine, if_exists='append', index=False)
