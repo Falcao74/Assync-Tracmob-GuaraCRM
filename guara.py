c1d141023bb96a528b65682d846b87f4a78904ed
Guaraci Falcão <53480221+Falcao74@users.noreply.github.com>
2023-11-08 09:15:32 -0300

    Update guara.py
    Refactored

--- guara.py
+++ guara.py
@@ -1,59 +1,59 @@
 import json
-from log import Log
-from asyncio import gather
-from typing import Dict, List
-from httpx import AsyncClient, ConnectError, ConnectTimeout
+import datetime
+from retry import retry
+import requests
 
-class Guara:
-    
+class GuaraCRM:
     def __init__(self, token):
-        self._endpoint: str = "https://guaracrm.com.br/api/v1/constituents"
-        self._headers: Dict = {'Content-Type': 'application/json'}
-        self._result_per_page: int = 50
-        self._token: str = token
-        self._start_page: int = 1
-        self._total_pages: int = 0
-        self._lista: List = []
-        
+        self._endpoint = "https://guaracrm.com.br/api/v1/constituents"
+        self._headers = {'Content-Type': 'application/json'}
+        self._page = 1
+        self._result_per_page = 60
+        self._token = token
+        self._total_pages = 0
+        self._chunk = 20
+    
     @property
     def url(self):
-        return f'{self._endpoint}?access_token={self._token}&per_page={self._result_per_page}&page=1'
+        return f'{self._endpoint}?access_token={self._token}&per_page={self._result_per_page}&page={self._page}'
     
-    async def set_total_pages(self) -> None:
-        async with AsyncClient() as client:
-            response = await client.get(self.url)
-            result = response.json()
-            self._total_pages = result['metadata']['pagination']['total_pages']
+    def has_next_page(self):
+        return False if self._page is None else True
+
+    @retry(exceptions=IOError, delay=30.0, tries=5)
+    def hit(self):
+        try:
+            with requests.Session() as client:
+                response = client.get(self.url, headers=self._headers)
+            if response.status_code != 200:
+                raise IOError
+            json = response.json()
+            self._page = json['metadata']['pagination']['next_page']
+            self._total_pages = json['metadata']['pagination']['total_pages']
+            return json['constituents']
+        
+        except Exception as error:
+            msg = f'Houve um erro no hit {str(error)} '
+            f = open("hit_erros.txt", "a")
+            tempo = datetime.datetime.now()
+            f.write(f'\n{tempo.hour}:{tempo.minute}:{tempo.second} - {msg}')
+            f.close()
+            raise IOError
     
-    def mount_requests(self, param: int) -> str:
-        return f'{self._endpoint}?access_token={self._token}&per_page=50&page={param}'
-    
-    async def create_tasks(self):
-        return [self.mount_requests(page) for page in range(self._start_page, self._total_pages + 1)]
-    
-    async def fetch_responses(self, tasks, retries = 3):
-        log = Log()
-        log.start_log()
-        for retry in range(retries):
-            try:
-                async with AsyncClient() as client:
-                    responses = await gather(*[client.get(task) for task in tasks])
-                    return responses
-            except (ConnectTimeout, ConnectError) as e:
-                log.add_event(f'Erro na tentativa {retry + 1}: {e}')
-            except Exception as e:
-                log.add_event(f'\n Surgiu um erro desconhecido no momento: tentativa {retry + 1 } \n  Requisitando os dados novamente: {e}')
-        log.add_event(f"Atingiu o número máximo de tentativas. Falha na solicitação.")
-        log.write_log(param = "requests_errors")
-        return []
-            
-    def process_data(self, responses):
-        for response in responses:
-            data = response.json()['constituents']
-            self._lista.extend(data)
-            
-    async def listagem(self):
-        tasks = await self.create_tasks()
-        responses = await self.fetch_responses(tasks)
-        self.process_data(responses)
-        return self._lista
+    def perform(self, chunk_size):
+        i = 0
+        data = []
+        while self.has_next_page() and i < chunk_size:
+            print(f'Pagina {self._page} de {self._total_pages} - {self.percentagem(self._page, self._total_pages):.2f} % do processo')
+            data += self.hit()
+            i += 1
+        return data
+
+    def percentagem(self, divisor, dividendo):
+        try:
+            _divisor = float(divisor)
+            _dividendo = float(dividendo)
+            result = 100 * _divisor / _dividendo
+            return result if isinstance(result, float) else 0.00
+        except ZeroDivisionError:
+            return 0.00
